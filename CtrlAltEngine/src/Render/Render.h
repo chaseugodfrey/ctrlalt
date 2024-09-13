077181641f80610ae6226d81fa65f07b592a68bd
Elton-SIT <2301455@sit.singaporetech.edu.sg>
2024-09-13 09:24:26 +0800

    Added Empty files for Render.h and .cpp

---
+++ CtrlAltEngine/src/Render/Render.h
@@ -0,0 +1,28 @@
+/******************************************************************************/
+/*!
+\group  	CtrlAlt
+\file		Render.h
+\author 	Elton Leosantosa
+\par    	leosantosa.e@digipen.edu
+\date   	Sep 13, 2024
+\brief		Render System Definitions and API
+
+
+Copyright (C) 2024 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+ */
+ /******************************************************************************/
+
+/// PREPROCESSOR DIRECTIVES
+///=========================================================================================================
+#ifndef ECS_H
+#define ECS_H
+
+namespace Render {
+
+
+
+}
+
+#endif
