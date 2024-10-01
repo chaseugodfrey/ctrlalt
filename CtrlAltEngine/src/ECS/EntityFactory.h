3056bfe9921cd3de861d02d71da827e6c4bc3807
visa21 <73213055+visa21@users.noreply.github.com>
2024-10-02 00:53:46 +0800

    Created Object factory

--- CtrlAltEngine/src/ECS/EntityFactory.h
+++ CtrlAltEngine/src/ECS/EntityFactory.h
@@ -5,6 +5,7 @@
 #include "../Components/CRigidBody.h"
 #include "../Components/CTransform.h"
 #include "../Scene/SceneManager.h"
+#include "../Logger/Logger.h"
 
 namespace ECS
 {
@@ -14,6 +15,29 @@ namespace ECS
 		Registry* registry;
 		Scene::SceneManager* sceneManager;
 	public:
+		EntityFactory(Registry* reg, Scene::SceneManager* sm) : registry(reg), sceneManager(sm) {}
+
+		Entity CreateBasicEntity()
+		{
+			Entity entity = registry->CreateEntity();
+			entity.AddComponent<Component::CTransform>();
+		}
+
+		Entity CreatePlayerEntity()
+		{
+			Entity entity = registry->CreateEntity();
+			entity.AddComponent<Component::CTransform>();
+			entity.AddComponent<Component::CRigidBody>();
+			entity.AddComponent<Component::CIdentifier>("Player");
+		}
+		
+		Entity CreateEnemyEntity()
+		{
+			Entity entity = registry->CreateEntity();
+			entity.AddComponent<Component::CTransform>();
+			entity.AddComponent<Component::CRigidBody>();
+			entity.AddComponent<Component::CIdentifier>("Enemy");
+		}
 	};
 }
 
