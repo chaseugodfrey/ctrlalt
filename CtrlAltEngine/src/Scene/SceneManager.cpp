cfad6b6ce0e13535602236e29cdcefcfe1519e34
chaseugodfrey <143150195+chaseugodfrey@users.noreply.github.com>
2024-10-19 06:49:30 +0800

    Moved Scene Manager h to cpp

---
+++ CtrlAltEngine/src/Scene/SceneManager.cpp
@@ -0,0 +1,140 @@
+/*!
+\group		CtrlAlt
+\file       SceneManager.cpp
+\author     Jerron Teng
+\par        t.junrongjerroni@digipen.edu
+\date       Sep 17, 2024
+\brief
+
+This file defines the `SceneManager` class, which is responsible for managing multiple scenes in an ECS system.
+It allows for adding, switching, and updating scenes, as well as serializing scene data.
+*/
+
+// INCLUDES
+// =========================================================================================================
+#include "SceneManager.h"
+#include <algorithm>
+#include "../ECS/ECS.h"
+#include "../Logger/Logger.h"
+#include "../Components/CRigidBody.h"
+#include "../Components/CTransform.h"
+#include "../Components/CIdentifier.h"
+
+namespace Scene
+{
+	/*!
+	@brief Constructor for SceneManager class
+	Special scenarios: -
+	@param -
+	@return -
+	\*________________________________________________________________*/
+	SceneManager::SceneManager(ECS::Registry* registry) : registry(registry), currentScene(nullptr) {
+
+	}
+
+	/*!
+	@brief Function call for Editor to make changes in Scene
+	Special scenarios: -
+	@param std::string& entityType
+	@return -
+	\*________________________________________________________________*/
+	void SceneManager::CreateEntityInScene(const std::string& entityType)
+	{
+		if (currentScene)
+		{
+			currentScene->CreateEntity(entityType);
+			currentScene->DebugPrintEntityCount();
+		}
+	}
+
+	/*!
+	@brief Function call to add a scene to the Editor given a filename and the filepath
+	Special scenarios: -
+	@param const std::string& name, const std::string& filePath
+	@return -
+	\*________________________________________________________________*/
+	void SceneManager::AddScene(const std::string& name, const std::string& filePath)
+	{
+		auto scene = std::make_unique<Scene>(registry, name, filePath);
+		scene->LoadEntityData();
+		scenes[name] = std::move(scene);
+		Logger::LogInfo("Scene added: " + name);
+	}
+
+	/*!
+	@brief Function to serialize data to a file, called by Editor
+	Special scenarios: -
+	@param -
+	@return -
+	\*________________________________________________________________*/
+	void SceneManager::SaveScene()
+	{
+		currentScene->SaveDataToFile();
+	}
+
+	/*!
+	@brief To remove a scene when given scene
+	Special scenarios: -
+	@param -
+	@return -
+	\*________________________________________________________________*/
+	void SceneManager::RemoveScene(const std::string& scene)
+	{
+		scenes.erase(scene);
+	}
+
+	/*!
+	@brief Editor to call this function to Switch scenes in given registry
+	Special scenarios: -
+	@param const std::string& sceneName
+	@return -
+	\*________________________________________________________________*/
+	void SceneManager::SwitchScene(const std::string& sceneName) {
+		auto it = scenes.find(sceneName);
+		if (it != scenes.end()) {
+			if (currentScene)
+				currentScene->Unload();
+			currentScene = it->second.get();
+			currentScene->Load();
+			Logger::LogInfo("Switched to scene: " + sceneName);
+			currentScene->DebugPrintEntityCount();
+		}
+		else {
+			Logger::LogInfo("Error, no scene: " + sceneName);
+		}
+	}
+
+	/*!
+	@brief Editor to call UpdateScene
+	Special scenarios: -
+	@param -
+	@return -
+	\*________________________________________________________________*/
+	void SceneManager::UpdateScene()
+	{
+		if (currentScene) currentScene->Update();
+	}
+
+	/*!
+	@brief Get current Scene name in the form of an std::string
+	Special scenarios: -
+	@param -
+	@return std::string
+	\*________________________________________________________________*/
+	std::string SceneManager::GetSceneNumber()
+	{
+		return currentScene->GetCurrentSceneName();
+	}
+
+	/*!
+	@brief Returns current Scene
+	Special scenarios: -
+	@param -
+	@return -
+	\*________________________________________________________________*/
+	Scene* SceneManager::GetScene()
+	{
+		return currentScene;
+	}
+
+}
--- CtrlAltEngine/src/Scene/SceneManager.h
+++ CtrlAltEngine/src/Scene/SceneManager.h
@@ -6,7 +6,7 @@
 \date       Sep 17, 2024
 \brief
 
-This file defines the `SceneManager` class, which is responsible for managing multiple scenes in an ECS system.
+This file declares the `SceneManager` class, which is responsible for managing multiple scenes in an ECS system.
 It allows for adding, switching, and updating scenes, as well as serializing scene data.
 */
 #ifndef SCENEMANAGER_H
@@ -19,7 +19,6 @@ It allows for adding, switching, and updating scenes, as well as serializing sce
 #include <string>
 #include "Scene.h"
 #include "../ECS/ECS.h"
-#include "../Logger/Logger.h"
 #include "../Components/CRigidBody.h"
 #include "../Components/CTransform.h"
 #include "../Components/CIdentifier.h"
@@ -40,9 +39,7 @@ namespace Scene
 		@param -
 		@return -
 		\*________________________________________________________________*/
-		SceneManager(ECS::Registry* registry) : registry(registry), currentScene(nullptr) {
-		
-		}
+		SceneManager(ECS::Registry*);
 
 		/*!
 		@brief Function call for Editor to make changes in Scene
@@ -50,14 +47,7 @@ namespace Scene
 		@param std::string& entityType
 		@return -
 		\*________________________________________________________________*/
-		void CreateEntityInScene(const std::string& entityType)
-		{
-			if (currentScene)
-			{
-				currentScene->CreateEntity(entityType);
-				currentScene->DebugPrintEntityCount();
-			}
-		}
+		void CreateEntityInScene(const std::string&);
 
 		/*!
 		@brief Function call to add a scene to the Editor given a filename and the filepath
@@ -65,13 +55,7 @@ namespace Scene
 		@param const std::string& name, const std::string& filePath
 		@return -
 		\*________________________________________________________________*/
-		void AddScene(const std::string& name, const std::string& filePath)
-		{
-			auto scene = std::make_unique<Scene>(registry, name, filePath);
-			scene->LoadEntityData();
-			scenes[name] = std::move(scene);
-			Logger::LogInfo("Scene added: " + name);
-		}
+		void AddScene(const std::string&, const std::string&);
 
 		/*!
 		@brief Function to serialize data to a file, called by Editor
@@ -79,10 +63,7 @@ namespace Scene
 		@param -
 		@return -
 		\*________________________________________________________________*/
-		void SaveScene()
-		{
-			currentScene->SaveDataToFile();
-		}
+		void SaveScene();
 
 		/*!
 		@brief To remove a scene when given scene
@@ -90,10 +71,7 @@ namespace Scene
 		@param -
 		@return -
 		\*________________________________________________________________*/
-		void RemoveScene(const std::string& scene)
-		{
-			scenes.erase(scene);
-		}
+		void RemoveScene(const std::string&);
 
 		/*!
 		@brief Editor to call this function to Switch scenes in given registry
@@ -101,20 +79,7 @@ namespace Scene
 		@param const std::string& sceneName
 		@return -
 		\*________________________________________________________________*/
-		void SwitchScene(const std::string& sceneName) {
-			auto it = scenes.find(sceneName);
-			if (it != scenes.end()) {
-				if (currentScene)
-				currentScene->Unload();
-				currentScene = it->second.get();
-				currentScene->Load();
-				Logger::LogInfo("Switched to scene: " + sceneName);
-				currentScene->DebugPrintEntityCount();
-			}
-			else {
-				Logger::LogInfo("Error, no scene: " + sceneName);
-			}
-		}
+		void SwitchScene(const std::string&);
 
 		/*!
 		@brief Editor to call UpdateScene
@@ -122,10 +87,7 @@ namespace Scene
 		@param -
 		@return -
 		\*________________________________________________________________*/
-		void UpdateScene()
-		{
-			if (currentScene) currentScene->Update();
-		}
+		void UpdateScene();
 
 		/*!
 		@brief Get current Scene name in the form of an std::string
@@ -133,10 +95,7 @@ namespace Scene
 		@param -
 		@return std::string 
 		\*________________________________________________________________*/
-		std::string GetSceneNumber()
-		{
-			return currentScene->GetCurrentSceneName();
-		}
+		std::string GetSceneNumber();
 
 		/*!
 		@brief Returns current Scene
@@ -144,11 +103,7 @@ namespace Scene
 		@param -
 		@return -
 		\*________________________________________________________________*/
-		Scene* GetScene()
-		{
-			return currentScene;
-		}
-
+		Scene* GetScene();
 
 	};
 }
